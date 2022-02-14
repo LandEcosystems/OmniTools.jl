bbee3491a594621cfbf11fcdfc8ddf28129ac159
lazarusA <lazarus.alon@gmail.com>
2022-02-14 09:32:49 +0100

    test core

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,11 @@
 using Sinbad
 using Test
-using Unitful
 
-# setup
-#o1 = rainSnow()
-#o2 = snowMelt()
-#o3 = snowMeltSimple()
+m1 = rainSnow()
+m2 = snowMelt()
+models = (m1, m2)
 
-#Model(o1)
-#run!(o1)
-# test operation with units and without units.
-# run full model altogether.
+forcing, timesteps = getforcing()
 
 m1 =  rainSnow()
 m2 = snowMelt()
@@ -34,4 +29,8 @@ forcing, timesteps = getforcing()
     @test m1.Tair_thres == 0.5
     @test m2.melt_T == 3.0
     @test size(outTime)[1] == n
+outTime = evolveEcosystem(forcing, models, timesteps)
+
+@testset "Sinbad.jl" begin
+    println("Hello, Sinbad?")
 end
