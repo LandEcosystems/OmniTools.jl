0d756c92ea621463eb4a4661307fb6107a152171
lazarusA <lazarus.alon@gmail.com>
2022-03-22 16:50:08 +0100

    rework

--- docs/src/index.md
+++ docs/src/index.md
@@ -13,28 +13,13 @@ Depth = 3
 
 Documentation for [Sinbad](https://git.bgc-jena.mpg.de/sindbad/sinbad.jl).
 
-## Package installation
-
-In the Julia REPL type:
+## Installing Sinbad
 
 ```julia
-julia> ]
+]
 pkg > add https://git.bgc-jena.mpg.de/sindbad/sinbad.jl.git
 ```
 
-The ] character starts the Julia package manager. Hit backspace key to return to Julia prompt.
-
-## Check installation
-
-Check Sinbad.jl version with:
-
 ```julia
-julia> ]
-pkg > st Sinbad
-```
-
-## Start using Sinbad
-
-```julia
-julia> using Sinbad
+using Sinbad
 ```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,10 @@
-using Revise
 using Sinbad
 using Test
 
-m1 = rainSnow()
-m2 = snowMelt()
-models = (m1, m2)
+using Sinbad.Models: rainSnow_Tair
 
-forcing, timesteps = getforcing()
+rainSnow_Tair()
 
-m1 =  rainSnow()
-m2 = snowMelt()
-m3 = evaporation()
-m4 = transpiration()
-mlast = updateState()
-forcing, timesteps = getforcing()
-#outTime = evolveEcosystem(forcing, models, timesteps)
-
-@testset "Sinbad.jl" begin
-    n = 100
-    m1 = rainSnow()
-    m2 = snowMelt()
-    models = (m1, m2)
-    variables = [:rain, :Tair, :Rn]
-    values = [rand(100), rand(100), rand(100)]
-    forcing = Table((; zip(variables, values)...))
-    timesteps = length(forcing)
-    outTime = runModels(forcing, models, timesteps)
-
-    @test m1.Tair_thres == 0.5
-    @test m2.melt_T == 3.0
-    @test size(outTime)[1] == n
-outTime = evolveEcosystem(forcing, models, timesteps)
-#forcing, timesteps = getforcing()
 m1 =  rainSnow()
 m2 = snowMelt()
 m3 = evapSoil()
@@ -49,7 +22,7 @@ forcing, timesteps = getforcing()
     values = [rand(100), rand(100), rand(100)]
     forcing = Table((; zip(variables, values)...))
     timesteps = length(forcing)
-    outTime = evolveEcosystem(forcing, models, timesteps)
+    outTime = runModels(forcing, models, timesteps)
 
     @test m1.Tair_thres == 0.5
     @test m2.melt_T == 3.0
