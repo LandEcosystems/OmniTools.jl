f158a2f3db602f5a19adfb94fd79849d42620949
lazarusA <lazarus.alon@gmail.com>
2022-04-25 10:23:58 +0200

    setting up docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,19 @@
 using Sinbad
 using Documenter
 
-DocMeta.setdocmeta!(Sinbad, :DocTestSetup, :(using Sinbad); recursive=true)
+#DocMeta.setdocmeta!(Sinbad, :DocTestSetup, :(using Sinbad); recursive=true)
 
 makedocs(;
-    modules=[Sinbad],
-    authors="lazarusA <lazarus.alon@gmail.com> and contributors",
-    repo="https://git.bgc-jena.mpg.de/sindbad/sinbad.jl/blob/{commit}{path}#{line}",
+    #modules=[Sinbad],
+    #authors="lazarusA <lazarus.alon@gmail.com> and contributors",
+    #repo="https://git.bgc-jena.mpg.de/sindbad/sinbad.jl/blob/{commit}{path}#{line}",
     sitename="Sinbad.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://git.bgc-jena.mpg.de/sindbad/sinbad.jl",
-        assets=String[]
-    ),
+    format=Documenter.HTML(prettyurls=true),
+    #format=Documenter.HTML(;
+    #    prettyurls=get(ENV, "CI", "false") == "true",
+    #    canonical="https://git.bgc-jena.mpg.de/sindbad/sinbad.jl",
+    #    assets=String[]
+    #),
     pages=[
         "Home" => "index.md",
         "Basic usage" => "basics.md"
