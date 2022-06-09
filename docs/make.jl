7fe73743ceebc6a0eac2e6d956628469d20fc33f
dr-ko <skoirala@bgc-jena.mpg.de>
2022-06-09 12:58:00 +0200

    add sindbad files

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using Sindbad
+using Documenter
+
+#DocMeta.setdocmeta!(Sindbad, :DocTestSetup, :(using Sindbad); recursive=true)
+
+makedocs(;
+    #modules=[Sindbad],
+    #authors="lazarusA <lazarus.alon@gmail.com> and contributors",
+    #repo="https://git.bgc-jena.mpg.de/sindbad/Sindbad.jl/blob/{commit}{path}#{line}",
+    sitename="Sindbad.jl",
+    format=Documenter.HTML(prettyurls=true),
+    #format=Documenter.HTML(;
+    #    prettyurls=get(ENV, "CI", "false") == "true",
+    #    canonical="https://git.bgc-jena.mpg.de/sindbad/Sindbad.jl",
+    #    assets=String[]
+    #),
+    pages=[
+        "Home" => "index.md",
+        "Basic usage" => "basics.md"
+    ]
+)
+#=
+deploydocs(;
+    repo="git.bgc-jena.mpg.de/sindbad/Sindbad.jl"
+)
+=#
---
+++ docs/src/index.md
@@ -0,0 +1,40 @@
+```@meta
+CurrentModule = Sindbad
+```
+
+## Table of Contents
+
+```@contents
+Pages = ["index.md", "basics.md"]
+Depth = 3
+```
+
+# Sindbad
+
+Documentation for [Sindbad](https://git.bgc-jena.mpg.de/sindbad/Sindbad.jl).
+
+## Package installation
+
+In the Julia REPL type:
+
+```julia
+julia> ]
+pkg > add https://git.bgc-jena.mpg.de/sindbad/Sindbad.jl.git
+```
+
+The ] character starts the Julia package manager. Hit backspace key to return to Julia prompt.
+
+## Check installation
+
+Check Sindbad.jl version with:
+
+```julia
+julia> ]
+pkg > st Sindbad
+```
+
+## Start using Sindbad
+
+```julia
+julia> using Sindbad
+```
