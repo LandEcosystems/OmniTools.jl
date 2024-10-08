2cf5954dca332df5285918888ec9a412715df0d2
Lazaro <lazarus.alon@gmail.com>
2024-10-08 14:00:57 +0200

    setup tests for Models

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,6 @@
 using Sindbad
+using BenchmarkTools
 using Test
 
-using Sindbad.Models: rainSnow_Tair
-
-rainSnow_Tair()
-
-m1 = rainSnow()
-m2 = snowMelt()
-m3 = evaporation()
-m4 = transpiration()
-mlast = updateState()
-forcing, timesteps = getforcing()
-#outTime = evolveEcosystem(forcing, models, timesteps)
-
-@testset "Sindbad.jl" begin
-    n = 100
-    m1 = rainSnow()
-    m2 = snowMelt()
-    models = (m1, m2)
-    variables = [:rain, :f_airT, :f_rn]
-    values = [rand(100), rand(100), rand(100)]
-    forcing = Table((; zip(variables, values)...))
-    timesteps = length(forcing)
-    outTime = computeTEM(forcing, models, timesteps)
-
-    @test m1.airT_thres == 0.5
-    @test m2.melt_T == 3.0
-    @test size(outTime)[1] == n
-end
+include("utilsCore.jl")
+include("Models/models.jl")
