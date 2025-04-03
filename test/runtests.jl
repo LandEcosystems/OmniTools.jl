8d0e2e71d2bdf0624deef396d806ac61d0dc0199
dr-ko <skoirala@bgc-jena.mpg.de>
2025-04-03 07:50:17 +0200

    remove defunct duplicates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,8 @@
-using Sinbad
+using Sindbad
+using BenchmarkTools
 using Test
 
-using Sinbad.Models: rainSnow_Tair
-
-rainSnow_Tair()
-
-m1 =  rainSnow()
-m2 = snowMelt()
-m3 = evaporation()
-m4 = transpiration()
-mlast = updateState()
-forcing, timesteps = getforcing()
-#outTime = evolveEcosystem(forcing, models, timesteps)
-
-@testset "Sinbad.jl" begin
-    n = 100
-    m1 = rainSnow()
-    m2 = snowMelt()
-    models = (m1, m2)
-    variables = [:rain, :Tair, :Rn]
-    values = [rand(100), rand(100), rand(100)]
-    forcing = Table((; zip(variables, values)...))
-    timesteps = length(forcing)
-    outTime = runModels(forcing, models, timesteps)
-
-    @test m1.Tair_thres == 0.5
-    @test m2.melt_T == 3.0
-    @test size(outTime)[1] == n
+@testset verbose=true begin
+    include("utilsCore.jl")
+    include("Models/models.jl") 
 end
